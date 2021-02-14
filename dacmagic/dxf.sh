1bcb7a28e0e813028c40138c402250aafb7b24a1
Callan Bryant <callan.bryant@gmail.com>
2021-02-14 10:26:33 +0000

    paths for macos

--- dacmagic/dxf.sh
+++ dacmagic/dxf.sh
@@ -4,5 +4,9 @@
 
 # https://github.com/openscad/openscad/pull/448
 set -e
+
+export PATH=$PATH:/usr/local/Caskroom/inkscape/1.0.2/Inkscape.app/Contents/MacOS/
+export PATH=$PATH:/usr/local/Caskroom/openscad/2021.01/OpenSCAD.app/Contents/MacOS/
+
 openscad parts/plate.scad -o plate.svg
 inkscape --export-filename=plate.dxf plate.svg --export-extension=org.ekips.output.dxf_outlines
