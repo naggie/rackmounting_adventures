361295e66e9dfaf83048f7db8831a5121daea5ad
Callan Bryant <callan.bryant@gmail.com>
2021-02-14 16:47:39 +0000

    fix dxf, target dist, add makefile

--- dacmagic/dxf2.sh
+++ dacmagic/dxf2.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-
 export PATH=$PATH:/usr/local/Caskroom/inkscape/1.0.2/Inkscape.app/Contents/MacOS/
 export PATH=$PATH:/usr/local/Caskroom/openscad/2021.01/OpenSCAD.app/Contents/MacOS/
 
+mkdir -p dist
+
 openscad parts/plate-vflip.scad -o plate-vflip.svg
-python3 svg_to_dxf.py plate-vflip.svg plate.dxf
+openscad parts/plate.scad -o dist/plate.svg
+python3 etc/svg_to_dxf.py plate-vflip.svg dist/plate.dxf
+rm plate-vflip.svg
