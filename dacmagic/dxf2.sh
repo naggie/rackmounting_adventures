0cad85274ec1529c6ba55746e0688889d813dae0
Callan Bryant <callan.bryant@gmail.com>
2021-02-15 21:52:00 +0000

    build other parts

--- dacmagic/dxf2.sh
+++ dacmagic/dxf2.sh
@@ -6,6 +6,15 @@ export PATH=$PATH:/usr/local/Caskroom/openscad/2021.01/OpenSCAD.app/Contents/Mac
 
 mkdir -p dist
 
+# 3d printed parts
+openscad parts/dac_bracket.scad -o dist/dac_bracket.stl
+openscad parts/switch_box.scad -o dist/switch_box.stl
+openscad parts/switch_box_lid.scad -o dist/switch_box_lid.stl
+
+# silkscreen
+openscad parts/silkscreen.scad -o dist/silkscreen.svg
+
+# plate
 openscad parts/plate-vflip.scad -o plate-vflip.svg
 openscad parts/plate.scad -o dist/plate.svg
 python3 etc/svg_to_dxf.py plate-vflip.svg dist/plate.dxf
